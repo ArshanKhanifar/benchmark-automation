a4c1babb7744acdecb7963327b8d5692e9a2b72e
Arshankhanifar <arshankhanifar@gmail.com>
2018-02-23 10:11:35 -0500

    oh-my-zsh installation

--- DeviceSetup.py
+++ DeviceSetup.py
@@ -19,7 +19,9 @@ class DeviceSetup(object):
             "sed -i .bak -e '/^#.*ASSUME_ALWAYS_YES /s/^#//' -e '/ASSUME_ALWAYS_YES /s/false/true/' /usr/local/etc/pkg.conf",
             "sed -i '.bak' -e 's/^mlxen/mlx4en/' /boot/loader.conf",
             "pkg install tmux vim-console git fzf zsh x86info yasm",
-            "chsh -s /usr/local/bin/zsh"
+            "chsh -s /usr/local/bin/zsh",
+            "sh -c \"$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)\""
+
         ]
         client = util.create_client(self.device_ip)
         util.execute_commands(client, commands_setup)
